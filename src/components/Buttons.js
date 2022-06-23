14f9d582512239ea2a914865bd51edbc9c6f4710
tomyang11 <tomyang1188@gmail.com>
2022-06-23 11:57:40 -0700

    Beginning

---
+++ src/components/Buttons.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Buttons = (props) => {
+
+}
+
+export default Buttons;
---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Display from './Display';
+import Buttons from './Buttons';
+
+const Calculator = () => {
+    return <React.Fragment>
+        <Display />
+        <Buttons />
+    </React.Fragment>
+}
+
+export default Calculator;
+
